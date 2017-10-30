f38ce78fcb33cdee138448f2676d725c9304f7f5
zabenno <benc853@gmail.com>
2017-10-31 02:31:02 +1300

    Removed unnecessary statements.

--- ReverseTCPClient/ReverseTCPClient/Program.cs
+++ ReverseTCPClient/ReverseTCPClient/Program.cs
@@ -1,25 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-
+﻿
 namespace ReverseTCPClient
 {
     class Program
     {
         static void Main(string[] args)
         {
-            CommandLine cmd = new CommandLine();
-            cmd.runCommand("echo hello");
-            cmd.runCommand("dir");
-            cmd.killCommandLine();
-            foreach(string line in cmd.outputRecieved)
-            {
-                Console.Out.WriteLine(line);
-            }
-            Thread.Sleep(3000);
+           
         }
     }
 }
