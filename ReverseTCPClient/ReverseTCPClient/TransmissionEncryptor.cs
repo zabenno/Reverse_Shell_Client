63c514b9bc46915d29aaf7222ab0dbea7014c74a
zabenno <benc853@gmail.com>
2017-10-31 02:27:28 +1300

    Added methods to support symmetric encryption.

--- ReverseTCPClient/ReverseTCPClient/TransmissionEncryptor.cs
+++ ReverseTCPClient/ReverseTCPClient/TransmissionEncryptor.cs
@@ -4,19 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
+using System.IO;
 
 namespace ReverseTCPClient
 {
     class TransmissionEncryptor
     {
+        /// <summary>
+        /// Setting up variables for asymmetric encryption.
+        /// </summary>
         private RSACryptoServiceProvider asymmetricForeign = new RSACryptoServiceProvider();
         private RSACryptoServiceProvider asymmetricLocal = new RSACryptoServiceProvider();
         public string localPublicString;
-        
+
+        /// <summary>
+        /// Setting up variables for symmetric encryption.
+        /// </summary>
+        private Aes aes = Aes.Create();
+        private List<byte[]> oldIVs = new List<byte[]>();
 
         public TransmissionEncryptor()
         {
             localPublicString = asymmetricLocal.ToXmlString(false);
+            aes.GenerateKey();
         }
         
         /// <summary>
@@ -107,6 +117,93 @@ namespace ReverseTCPClient
             Array.Copy(signedData, data, signedData.Length - 129);
             Array.Copy(signedData, signedData.Length - 129, signiture, 0, 128);
             return new Tuple<byte[], byte[]>(data, signiture);
+        }
+
+        /// <summary>
+        /// Sets the key to be used by AES for encryption.
+        /// </summary>
+        /// <param name="key">The Key for AES encryption</param>
+        public void setAESKey(byte[] key)
+        {
+            aes.Key = key;
+        }
+
+        /// <summary>
+        /// Encrypts a byte[] object using the set key and a IV that is appended to the start of the output.
+        /// </summary>
+        /// <param name="plainText">The byte[] object to be encrypted.</param>
+        /// <returns>The IV that has been used appended by the cipher text.</returns>
+        public byte[] encryptAES(byte[] plainText)
+        {
+            //Generating random IV key for one time use.
+            generateUniqueIV();
+            byte[] encryptedMessage;
+            //Creating encryptor.
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+            using (MemoryStream memStream = new MemoryStream())
+            {
+                using(CryptoStream cryptStream = new CryptoStream(memStream, encryptor, CryptoStreamMode.Write))
+                {
+                    cryptStream.Write(plainText, 0, plainText.Length);
+                }
+                encryptedMessage = memStream.ToArray();
+            }
+
+            //Adding IV to the start of the cipher text.
+            byte[] datagram = new byte[aes.IV.Length+encryptedMessage.Length];
+            Array.Copy(aes.IV, datagram, aes.IV.Length);
+            Array.Copy(encryptedMessage, 0, datagram, aes.IV.Length, encryptedMessage.Length);
+
+            return datagram;
+        }
+
+        /// <summary>
+        /// Decrypts a cipher text using AES with the given key of the instance and the IV that is appended to the start of the datagram. 
+        /// </summary>
+        /// <param name="datagram">IV appended with the cipher text.</param>
+        /// <returns>Byte[] object of plain text.</returns>
+        public byte[] decryptAES(byte[] datagram)
+        {
+            byte[] plainText;
+            
+            //Seperating the IV from the cipher text.
+            byte[] IV = new byte[aes.IV.Length];
+            byte[] cipherText = new byte[datagram.Length - aes.IV.Length];
+            Array.Copy(datagram, 0, IV, 0, aes.IV.Length);
+            Array.Copy(datagram, aes.IV.Length, cipherText, 0, datagram.Length - aes.IV.Length);
+
+            //Creating the decryptor.
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, IV);
+
+            using(MemoryStream memStream = new MemoryStream())
+            {
+                using(CryptoStream cryptStream = new CryptoStream(memStream, decryptor, CryptoStreamMode.Write))
+                {
+                    cryptStream.Write(cipherText, 0, cipherText.Length);
+                }
+                plainText = memStream.ToArray();
+            }
+
+            return plainText;
+        }
+
+        /// <summary>
+        /// Generates new IVs until one that has not been used is found.
+        /// </summary>
+        private void generateUniqueIV()
+        {
+            bool isNotUnique = true;
+
+            while (isNotUnique)
+            {
+                aes.GenerateIV();
+                if (!oldIVs.Contains(aes.IV))
+                {
+                    isNotUnique = false;
+                }
+                
+            }
         }
     }
 }
